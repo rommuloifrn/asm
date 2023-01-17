90a897dfbafc2455738ea9f4f3f8aefc2fa409a7
Rômulo da Silva Cavalcanti <20221014040015@ifrn.local>
2023-01-17 14:28:18 -0300

    created game directory and fixed gradient function of display.asm

---
+++ game/display.asm
@@ -0,0 +1,136 @@
+# A program i made to learn about display and graphical interface.
+.data
+.word 
+
+
+.text
+main:
+	lui $15 0x1001		# Stores 0x1001 (start adress) on the upper decimal places of $15
+	addi $9 $0 32768
+	addi $8 $0 0x000000	# Stores a sky color in $8
+	jal bgaaa
+	
+	lui $15 0x1001
+	
+	add $4 $0 $15 		# Stores $15 as padress point zero
+	addi $5 $0 55		# Stores 45 as padress line
+	addi $6 $0 128		# Stores 128 as padress line graphical unit amount
+	addi $7 $0 0		# Stores 0 as padress column
+	jal padress
+	
+	addi $8 $0 0x684e22	# Stores a shade of brown in $8
+	add $15 $0 $2		# Stores the result of padress as the first pixel
+	addi $9 $0 32768	# Stores rect last pixel
+	jal rect
+	
+	
+	
+	
+
+end:
+	addi $2 $0 10
+	syscall
+
+# Module to change the background of the 512x256 display
+# Input: $8 as the color
+# 	 $15 as the pixel
+#	 $9 as the pixel amount
+bg:
+	sw $8 0($15)
+	addi $15 $15 4
+	addi $9 $9 -4
+	bne $9 0 bg
+	jr $31
+	
+# Instance of bg
+# Input: $8 as the color
+# 	 $15 as the pixel
+#	 $9 as the pixel amount
+#	 $10 as pixel counter to the color addition
+# 	 $11 as line counter to the color addition
+bgaaa:
+	sw $8 0($15)
+	addi $15 $15 4
+	addi $9 $9 -4
+	
+	addi $10 $10 1
+	beq $11 42 comeback
+	beq $10 128 grad
+	comeback:
+	bne $9 0 bgaaa
+	jr $31
+	
+	grad:
+	addi $10 $0 0
+	addi $11 $11 1
+	addi $8 $8 0x020406
+	j comeback
+	
+	
+# Module to change the background of the 512x256 display, with gradient
+# Input: $8 as the color
+# 	 $15 as the pixel
+#	 $9 as the pixel amount
+# 	 $6 as line width (graphical units)
+gbg:
+	sw $8 0($15)
+	addi $15 $15 4
+	addi $9 $9 -4
+	div $15 $6
+	mfhi $13
+	beq $13 0 color
+	colorcontinue:
+	bne $9 0 gbg
+	jr $31
+	color:
+	addi $8 $8 0x0010
+	j colorcontinue
+	
+# strange instance of gbg
+gbga:
+	sw $8 0($15)
+	addi $15 $15 4
+	addi $9 $9 -4
+	
+	div $15 $6
+	mfhi $13
+	slti $13 $13 2
+	add $8 $8 $13
+	
+	bne $9 0 gbga
+	jr $31
+	
+
+# Module to print a massive rectangle from 2 pixels ($15 and $9)
+# Input: $8 as the color
+# 	 $15 as the first pixel
+#	 $9 as the last pixel
+rect:
+	sw $8 0($15)
+	addi $15 $15 4
+	addi $9 $9 -4
+	bne $9 0 rect
+	jr $31
+	
+
+
+
+# Module to calculate the adress of a point (l, c)
+# Input: $4 Point zero
+# 	 $5 Line
+#	 $6 Line amount of graphical units
+#	 $7 Column
+# Output:$2
+# Uses (without preserving)
+# Obs.: Doesn't check parameters
+padress: mul $8 $5 $6	# Stores the multiplication of line by line graphical units amount
+	 add $8 $8 $7 	# Sums $8 with $7 (column)
+	 sll $8 $8 2	# Multiplies $8 by 4
+	 add $2 $8 $4	# Sums $8 with point zero
+	 jr $31
+	 
+
+	 
+	 
+	 
+	 
